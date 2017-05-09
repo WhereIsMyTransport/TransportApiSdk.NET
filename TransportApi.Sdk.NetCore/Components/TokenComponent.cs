985044ca4881c8d59e3e000d3943e3578fbd432f
Jan <jan@whereismytransport.com>
2017-05-09 14:30:12 +0200

    Added NetCore project

---
+++ TransportApi.Sdk.NetCore/Components/TokenComponent.cs
@@ -0,0 +1,78 @@
+﻿using IdentityModel.Client;
+using System;
+using System.Threading.Tasks;
+using TransportApi.Sdk;
+using TransportApi.Sdk.Interfaces;
+
+namespace TransportApi.Sdk.Components
+{
+    internal sealed class TokenComponent : ITokenComponent
+    {
+        private TokenResponse token;
+        private DateTime expiryTime;
+        private string clientId;
+        private string clientSecret;
+
+        public string DefaultErrorMessage
+        {
+            get
+            {
+                return "Unable to generate access token. Please check your credentials.";
+            }
+        }
+
+        public TokenComponent(string clientId, string clientSecret)
+        {
+            if (string.IsNullOrWhiteSpace(clientId))
+            {
+                throw new ArgumentNullException(nameof(clientId), "ClientId cannot be null");
+            }
+
+            if (string.IsNullOrWhiteSpace(clientSecret))
+            {
+                throw new ArgumentNullException(nameof(clientSecret), "ClientSecret cannot be null");
+            }
+
+            this.clientId = clientId;
+            this.clientSecret = clientSecret;
+        }
+
+        public async Task<string> GetAccessToken()
+        {
+            if (token != null)
+            {
+                if (expiryTime > DateTime.UtcNow)
+                {
+                    return token.AccessToken;
+                }
+            }
+
+            var client = new TokenClient(
+                TransportApiClientConnection.IdentityStsTokenUri.ToString(),
+                clientId,
+                clientSecret);
+
+            var scopes = "transportapi:all";
+
+            try
+            {
+                var tokenResponse = await client.RequestClientCredentialsAsync(scopes);
+
+                if (tokenResponse.IsError || tokenResponse.IsError)
+                {
+                    return null;
+                }
+
+                token = tokenResponse;
+                expiryTime = DateTime.UtcNow.AddSeconds(token.ExpiresIn);
+
+                return token.AccessToken;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+
+        }
+    }
+}
---
+++ TransportApi.Sdk.NetCore/RestExtensions.cs
@@ -0,0 +1,60 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TransportApi.Sdk.NetCore
+{
+    public static class RestExtensions
+    {
+        public static async Task<IRestResponse<T>> ExecuteTaskAsync<T>(this RestClient client, IRestRequest request, CancellationToken token)
+        {
+            if (request == null)
+            {
+                throw new ArgumentNullException("request");
+            }
+
+            TaskCompletionSource<IRestResponse<T>> taskCompletionSource = new TaskCompletionSource<IRestResponse<T>>();
+
+            try
+            {
+                RestRequestAsyncHandle async = client.ExecuteAsync<T>(
+                    request,
+                    (response, _) =>
+                    {
+                        if (token.IsCancellationRequested)
+                        {
+                            taskCompletionSource.TrySetCanceled();
+                        }
+                        // Don't run TrySetException, since we should set Error properties and swallow exceptions
+                        // to be consistent with sync methods
+                        else
+                        {
+                            taskCompletionSource.TrySetResult(response);
+                        }
+                    });
+
+
+                CancellationTokenRegistration registration =
+
+                    token.Register(() =>
+                    {
+                        async.Abort();
+                        taskCompletionSource.TrySetCanceled();
+                    });
+
+
+                taskCompletionSource.Task.ContinueWith(t => registration.Dispose(), token);
+
+            }
+            catch (Exception ex)
+            {
+                taskCompletionSource.TrySetException(ex);
+            }
+
+            return await taskCompletionSource.Task;
+        }
+    }
+}
